ca6b916b834aff1a34a120bf7441d36577a51680
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-12-01 14:30:59 -0600

    feat(general): Set up basic GUI render - initial

---
+++ GUI.java
@@ -0,0 +1,26 @@
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+
+public class GUI {
+
+    public GUI() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Counter GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
