dd68303715a70a969646bd87681d1c8a38225e8c
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-12-01 14:45:07 -0600

    feat(core): Implemet counter + func

--- GUI.java
+++ GUI.java
@@ -1,17 +1,32 @@
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    int count = 0;
+    JLabel label;
+    JFrame frame;
+    JPanel panel;
+    JButton button;
 
     public GUI() {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        frame = new JFrame();
+        button = new JButton("Click here");
+        button.addActionListener(this);
+        panel = new JPanel();
+        label = new JLabel("Number of clicks: 0");
 
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,5 +37,12 @@ public class GUI {
 
     public static void main(String[] args) {
         new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+
     }
 }
